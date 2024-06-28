058a01431ed93ff1cf825c982001d9f1d5f56918
keunpyo jeon <john.coner72@gmail.com>
2024-06-28 21:04:45 +0900

    first-commit

---
+++ main.js
@@ -0,0 +1,30 @@
+let names = ["steven paul jobs",
+    "bill gates",
+    "mark elliot zuckerberg",
+    "elon musk",
+    "warren edward buffet",
+    "larry page",
+    "larry ellison",
+    "tim cook",
+    "lioyd blanckfein"
+]
+
+
+
+
+/*
+1. let data = names.map((b)=>{
+    return b.toUpperCase()
+})
+
+console.log(data)
+
+2.names.forEach(element => {console.log(element.split(' ',1))
+    
+});
+*/
+let data2 = names.filter((b)=>{
+    return b === 'a'
+})
+
+console.log(data2)
